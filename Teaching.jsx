513ee6ce3412d734d84474915a873fdeac477fed
aadii1502 <129250086+aadii1502@users.noreply.github.com>
2023-10-05 04:55:33 +0530

    Add files via upload

---
+++ Teaching.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+
+export default function Teaching() {
+  return (
+    <div className=' grid grid-rows-3 grid-cols-4 gap-x-4 my-6 mx-2'>
+    <div className="ratio ratio-16x9">
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/UzMaLV1Dpk4?si=1XsWhAwvYsOqAjeS" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    </div>
+
+    <div className='ratio ratio-21x9'>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/6w1ZDaE-whc?si=Bh4IrDCfBeJpI8aO" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    </div>
+
+    <div className='ratio ratio-21x9'>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/bFv_mLwBvHc?si=ZZwF_0EAUvLifYv5" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    </div>
+
+    <div className='ratio ratio-21x9'>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/4Ll3OtqAzyw?si=HewZKhLQpq0tcjwQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    </div>
+
+
+    <div className="ratio ratio-16x9 my-3">
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/UzMaLV1Dpk4?si=1XsWhAwvYsOqAjeS" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    </div>
+
+    <div className='ratio ratio-21x9 my-3'>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/6w1ZDaE-whc?si=Bh4IrDCfBeJpI8aO" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    </div>
+
+    <div className='ratio ratio-21x9 my-3'>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/bFv_mLwBvHc?si=ZZwF_0EAUvLifYv5" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    </div>
+
+    <div className='ratio ratio-21x9 my-3'>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/4Ll3OtqAzyw?si=HewZKhLQpq0tcjwQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    </div>
+
+    <div className="ratio ratio-16x9">
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/UzMaLV1Dpk4?si=1XsWhAwvYsOqAjeS" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    </div>
+
+    <div className='ratio ratio-21x9'>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/6w1ZDaE-whc?si=Bh4IrDCfBeJpI8aO" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    </div>
+
+    <div className='ratio ratio-21x9'>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/bFv_mLwBvHc?si=ZZwF_0EAUvLifYv5" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    </div>
+
+    <div className='ratio ratio-21x9'>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/4Ll3OtqAzyw?si=HewZKhLQpq0tcjwQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    </div>
+
+    </div>
+  );
+}
